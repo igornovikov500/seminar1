e01c2cae96c33109751efb51f7ff4620b47f7a72
Игорь Новиков <rshb123@mail.ru>
2022-09-11 16:07:53 +0300

    конечный вариант

---
+++ hv_seminar/seminar3/task22/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 22: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+// int count = 1;
+// while (count <= number)
+// {  
+//   int kvadrant = Convert.ToInt32(Math.Pow(count,2));
+//   Console.Write(kvadrant + " ");
+//   count++;
+// }
+if (number > 0)
+{
+    for (int i = 1; i <= number; i++)
+        {
+        Console.Write(i*i + " ");
+        }
+}
+else
+{
+  Console.WriteLine("число должно быть положительное");
+}
